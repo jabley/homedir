1ed69f3493abdbce2a1aed3db11b1f414a2bf96a
James Abley <james.abley@gmail.com>
2012-08-22 09:01:47 +0100

    Detect presence of EC2 security files
    Fix error in .bashrc running when the files aren't present.

--- .bashrc.d/ec2.bash
+++ .bashrc.d/ec2.bash
@@ -1,9 +1,12 @@
 # Commented out JAVA_HOME for now, since it seems to interfere with /usr/bin/java
 #export JAVA_HOME="/System/Library/Frameworks/JavaVM.framework/Home"
-export EC2_PRIVATE_KEY="$(/bin/ls $HOME/.ec2/pk-55*.pem)"
-export EC2_CERT="$(/bin/ls $HOME/.ec2/cert-55*.pem)"
-export EC2_HOME="/usr/local/Library/LinkedKegs/ec2-api-tools/jars"
 
+if [ -f "$HOME/.ec2/pk-55*.pem" ]; then
+  export EC2_PRIVATE_KEY="$(/bin/ls $HOME/.ec2/pk-55*.pem)"
+  export EC2_CERT="$(/bin/ls $HOME/.ec2/cert-55*.pem)"
+fi
+
+export EC2_HOME="/usr/local/Library/LinkedKegs/ec2-api-tools/jars"
 export AWS_IAM_HOME="/usr/local/Cellar/aws-iam-tools/HEAD/jars"
 export AWS_CREDENTIAL_FILE=$HOME/.aws-credentials-master
 
