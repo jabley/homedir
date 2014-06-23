9e1785b0c9cd1a684f8d8f70ca647e68b3e29409
James Abley <james.abley@gmail.com>
2014-06-23 09:27:42 +0100

    Change $PATH for more golang goodness

--- .bashrc.d/go.bash
+++ .bashrc.d/go.bash
@@ -1,4 +1,6 @@
 #!/bin/sh
 export GOPATH=~/gocode
 export PATH="$PATH:$GOPATH/bin"
-
+# Add the GOROOT-based install location to your PATH so that `godoc` and
+# `go vet` will work
+export PATH=$PATH:/opt/boxen/homebrew/opt/go/libexec/bin
