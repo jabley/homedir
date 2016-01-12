9102db4a8297bdaf86607ede8c702e9476872277
James Abley <james.abley@gmail.com>
2016-01-12 09:48:46 +0000

    Configure emacs properly

---
+++ .bashrc.d/emacs.bash
@@ -0,0 +1,4 @@
+# Ensure that latest emacsclient is first in the PATH
+export PATH="/Applications/Emacs.app/Contents/MacOS/bin:${PATH}"
+# Point emacs at latest version
+alias emacs=/Applications/Emacs.app/Contents/MacOS/Emacs
