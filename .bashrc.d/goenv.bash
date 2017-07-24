4fec5db5c8e3af536a5d3a2d0aed601711abafe9
James Abley <james.abley@gmail.com>
2017-07-24 11:10:39 +0100

    Add FlameGraph to PATH if present
    See github.com/jabley/our-boxen@a9cfbff

--- .bashrc.d/goenv.bash
+++ .bashrc.d/goenv.bash
@@ -3,3 +3,7 @@ if [ -d "${HOME}/.goenv" ]; then
     export PATH="$HOME/.goenv/bin:$PATH"
     eval "$(goenv init -)"
 fi
+
+if [ -d "${HOME}/Projects/brendangregg/FlameGraph" ]; then
+    export PATH="${PATH}:${HOME}/Projects/brendangregg/FlameGraph"
+fi
